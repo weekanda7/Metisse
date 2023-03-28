02b025afe9b6dbb83620b35bf855398a914ae86a
weekanda7 <87613475+weekanda7@users.noreply.github.com>
2023-03-28 14:59:33 +0800

    fix path bug

--- autoscript_kernel/metis.py
+++ autoscript_kernel/metis.py
@@ -12,6 +12,7 @@ import io
 import logging
 import os
 import time
+import sys
 try:
     from PyQt6 import QtWidgets
     from PyQt6.QtGui import QImage, QPixmap
@@ -29,6 +30,9 @@ from cv2 import Mat
 from PIL import Image
 import natsort
 from google.cloud import vision  # type: ignore
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
 from .params import ImageRecognitionParams, SaveParams
 from .template_metis import TemplateMetis
 
