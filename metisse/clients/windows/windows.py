d30b53d1916e3f6ee1aa4f0fb39f0e2af7d65275
weekanda7 <87613475+weekanda7@users.noreply.github.com>
2023-04-27 15:32:27 +0800

    fix setting bug , add windows client func

--- metisse/clients/windows/windows.py
+++ metisse/clients/windows/windows.py
@@ -1,12 +1,10 @@
 # -*- coding=UTF-8 -*-
 
-from typing import Tuple
+from typing import List, Tuple
 from mss import mss
 import win32api
 import win32con
 import win32gui
-import win32ui
-from ctypes import windll
 from PIL import Image
 import pygetwindow as gw
 import time
@@ -22,7 +20,24 @@ class WindowsClient(Client):
         self.target_window = gw.getWindowsWithTitle(self.window_title)[0]
         self.target_window.restore()  # 将窗口恢复到正常状态（即非最小化状态）
         self.target_window.moveTo(0, 0)  # 将窗口移动到屏幕的 (0, 0) 坐标
-        self.target_window.resizeTo(screen_size)  # 将窗口调整为 (640, 1080) 大小
+        self.target_window.resizeTo(screen_size[0], screen_size[1])  # 将窗口调整为 (640, 1080) 大小
+    @staticmethod
+    def get_title() -> List[List[str]]:
+        title_list = []
+        def callback(hwnd, titles):
+            if win32gui.IsWindowVisible(hwnd):
+                window_title = win32gui.GetWindowText(hwnd)
+                if window_title:
+                    titles.append((hwnd, window_title))
+        titles = []
+        win32gui.EnumWindows(callback, titles)
+
+        for hwnd, title in titles:
+            title_list.append([f"Handle: {hwnd}, Title: {title}"])
+        return title_list
+
+    def resize(self, screen_size: Tuple[int, int]=(640, 1080)) -> None:
+        self.target_window.resizeTo(screen_size[0], screen_size[1])
 
     def screenshot(self, save_screenshot_path: str) -> None:
         self.set_front()  # 确保窗口在前景
@@ -31,7 +46,7 @@ class WindowsClient(Client):
             monitor = {"top": window_rect[1], "left": window_rect[0], "width": window_rect[2] - window_rect[0], "height": window_rect[3] - window_rect[1]}
             img = sct.grab(monitor)
             img_pil = Image.frombytes("RGB", img.size, img.bgra, "raw", "BGRX")
-            img_pil.save(save_screenshot_path)
+            img_pil.save(save_screenshot_path,dpi=(90, 90))
 
     def tap(self, coordinates: Tuple[int, int]) -> None:
         self.set_front()
@@ -41,9 +56,10 @@ class WindowsClient(Client):
         window_rect = win32gui.GetWindowRect(hWnd)
         border_thickness = int((window_rect[2] - window_rect[0] - client_rect[2]) / 2)
         title_bar_height = window_rect[3] - window_rect[1] - client_rect[3] - border_thickness
-
+        print(window_rect[0],window_rect[1])
+        print(border_thickness,title_bar_height)
         adjusted_x = x + window_rect[0] + border_thickness
-        adjusted_y = y + window_rect[1] + title_bar_height
+        adjusted_y = y + window_rect[1] # title_bar_height
         win32api.SetCursorPos((adjusted_x, adjusted_y))
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
--- metisse/settings.py
+++ metisse/settings.py
@@ -1,4 +1,4 @@
 class Settings(object):
     DEV_ENVIRONMENT_ROOT_PATH = ['icon', 'ui']  #,'script'
     SCRIPT_ENVIRONMENT_ROOT_PATH = ['temp_image', 'storage', 'backup', 'log']
-    OS_ENVIRONMENT = ['android', 'ios']
+    OS_ENVIRONMENT = ['android', 'ios','windows']
